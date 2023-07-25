620e41dfce732980537befc74f79fe5cf0ba64f2
Federico Cicciarella <federico.cicciarella@gmail.com>
2023-07-25 20:49:11 +0200

    refactory

---
+++ src/Substrate.Shibuya.NET.Client/Client/BaseClient.cs
@@ -0,0 +1,173 @@
+﻿using Schnorrkel.Keys;
+using Serilog;
+using StreamJsonRpc;
+using Substrate.NetApi;
+using Substrate.NetApi.Model.Extrinsics;
+using Substrate.NetApi.Model.Types;
+using Substrate.Shibuya.NET.NetApiExt.Generated;
+using Substrate.Shibuya.NET.NetApiExt.Generated.Storage;
+
+namespace Shibuya.Integration.Client
+{
+    public class BaseClient
+    {
+        private readonly int _maxConcurrentCalls;
+
+        private readonly ChargeType _chargeTypeDefault;
+
+        public static MiniSecret MiniSecretAlice => new MiniSecret(Utils.HexToByteArray("0xe5be9a5092b81bca64be81d212e7f2f9eba183bb7a90954f7b76361f6edb5c0a"), ExpandMode.Ed25519);
+        public static Account Alice => Account.Build(KeyType.Sr25519, MiniSecretAlice.ExpandToSecret().ToBytes(), MiniSecretAlice.GetPair().Public.Key);
+
+        public ExtrinsicManager ExtrinsicManger { get; }
+
+        public SubscriptionManager SubscriptionManager { get; }
+
+        public SubstrateClientExt SubstrateClient { get; }
+
+        public bool IsConnected => SubstrateClient.IsConnected;
+
+        public BaseClient(string url, int maxConcurrentCalls = 10)
+        {
+            _chargeTypeDefault = ChargeTransactionPayment.Default();
+            //_chargeTypeDefault = ChargeAssetTxPayment.Default();
+
+            _maxConcurrentCalls = maxConcurrentCalls;
+
+            SubstrateClient = new SubstrateClientExt(new Uri(url), _chargeTypeDefault);
+
+            ExtrinsicManger = new ExtrinsicManager();
+
+            SubscriptionManager = new SubscriptionManager();
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="useMetadata"></param>
+        /// <param name="standardSubstrate"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public async Task<bool> ConnectAsync(bool useMetadata, bool standardSubstrate, CancellationToken token)
+        {
+            if (!IsConnected)
+            {
+                try
+                {
+                    await SubstrateClient.ConnectAsync(useMetadata, standardSubstrate, token);
+                }
+                catch (Exception e)
+                {
+                    Log.Error("BaseClient.ConnectAsync: {0}",
+                        e.ToString());
+                }
+            }
+
+            return IsConnected;
+        }
+
+        public async Task<bool> DisconnectAsync()
+        {
+            if (!IsConnected)
+            {
+                return false;
+            }
+
+            await SubstrateClient.CloseAsync();
+            return true;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="extrinsicType"></param>
+        /// <param name="extrinsicMethod"></param>
+        /// <param name="concurrentTasks"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        internal async Task<string> GenericExtrinsicAsync(Account account, string extrinsicType, Method extrinsicMethod, int concurrentTasks, CancellationToken token)
+        {
+            if (account == null)
+            {
+                Log.Warning("Account is null!");
+                return null;
+            }
+
+            if (!IsConnected)
+            {
+                Log.Warning("Currently not connected to the network!");
+                return null;
+            }
+
+            if (ExtrinsicManger.Running.Count() >= _maxConcurrentCalls)
+            {
+                Log.Warning("There can not be more then {0} concurrent tasks overall!", _maxConcurrentCalls);
+                return null;
+            }
+
+            if (ExtrinsicManger.Running.Count(p => p.ExtrinsicType == extrinsicType) >= concurrentTasks)
+            {
+                Log.Warning("There can not be more then {0} concurrent tasks of {1}!", concurrentTasks, extrinsicType);
+                return null;
+            }
+
+            string subscription = null;
+            try
+            {
+                subscription = await SubstrateClient.Author.SubmitAndWatchExtrinsicAsync(ExtrinsicManger.ActionExtrinsicUpdate, extrinsicMethod, account, _chargeTypeDefault, 64, token);
+            }
+            catch (RemoteInvocationException e)
+            {
+                Log.Error("RemoteInvocationException: {0}", e.Message);
+                return subscription;
+            }
+
+            if (subscription == null)
+            {
+                return null;
+            }
+
+            Log.Debug("Generic extrinsic sent {0} with {1}.", extrinsicMethod.ModuleName + "_" + extrinsicMethod.CallName, subscription);
+
+            ExtrinsicManger.Add(subscription, extrinsicType);
+
+            return subscription;
+        }
+
+        public async Task<string> SubscribeEventsAsync(CancellationToken token)
+        {
+            if (!IsConnected)
+            {
+                Log.Warning("Currently not connected to the network!");
+                return null;
+            }
+
+            if (SubscriptionManager.IsSubscribed)
+            {
+                Log.Warning("Already active subscription to events!");
+                return null;
+            }
+
+            var subscription = await SubstrateClient.SubscribeStorageKeyAsync(SystemStorage.EventsParams(), SubscriptionManager.ActionSubscrptionEvent, token);
+            if (subscription == null)
+            {
+                return null;
+            }
+
+            SubscriptionManager.IsSubscribed = true;
+
+            Log.Debug("SystemStorage.Events subscription id {0] registred.", subscription);
+
+            return subscription;
+        }
+
+        public static Account RandomAccount(int seed, string derivationPsw = "aA1234dd", KeyType keyType = KeyType.Sr25519)
+        {
+            var random = new Random(seed);
+            var randomBytes = new byte[16];
+            random.NextBytes(randomBytes);
+            var mnemonic = string.Join(" ", Mnemonic.MnemonicFromEntropy(randomBytes, Mnemonic.BIP39Wordlist.English));
+            Log.Information("mnemonic[Sr25519]: {0}", mnemonic);
+            return Mnemonic.GetAccountFromMnemonic(mnemonic, derivationPsw, keyType);
+        }
+    }
+}
---
+++ src/Substrate.Shibuya.NET.Client/ShibuyaNetwork.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
+using Shibuya.Integration.Client;
+using Substrate.NetApi;
+using Substrate.NetApi.Model.Types;
+using Substrate.NetApi.Model.Types.Base;
+using Substrate.NetApi.Model.Types.Primitive;
+using Substrate.Shibuya.NET.NetApiExt.Generated.Model.shibuya_runtime;
+using Substrate.Shibuya.NET.NetApiExt.Generated.Model.frame_system;
+using Substrate.Shibuya.NET.NetApiExt.Generated.Model.sp_core.crypto;
+
+using Substrate.Shibuya.NET.NetApiExt.Generated.Model.sp_runtime.multiaddress;
+using Substrate.Shibuya.NET.NetApiExt.Generated.Storage;
+using Serilog;
+using Shibuya.Integration.Helper;
+using Substrate.Shibuya.NET.NetApiExt.Generated.Model.sp_weights.weight_v2;
+
+namespace Shibuya.Integration
+{
+    public partial class ShibuyaNetwork : BaseClient
+    {
+        public Account Account { get; set; }
+
+        public ShibuyaNetwork(Account account, string url) : base(url)
+        {
+            Account = account;
+        }
+
+        /// <summary>
+        /// Get the current block number
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public async Task<U32?> GetBlocknumberAsync(CancellationToken token)
+        {
+            if (!IsConnected)
+            {
+                Log.Warning("Currently not connected to the network!");
+                return null;
+            }
+
+            return await SubstrateClient.SystemStorage.Number(token);
+        }
+
+        /// <summary>
+        /// Get the account infos for the current account
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public async Task<AccountInfo> GetAccountAsync(CancellationToken token)
+        {
+            if (Account == null || Account.Value == null)
+            {
+                Log.Warning("No account set!");
+                return null;
+            }
+
+            return await SubstrateClient.SystemStorage.Account(Account.Value.ToAccountId32(), token);
+        }
+
+        /// <summary>
+        /// Execute a transfer keep alive extrinsic
+        /// </summary>
+        /// <param name="to"></param>
+        /// <param name="amount"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public async Task<string?> TransferKeepAliveAsync(AccountId32 to, BigInteger amount, int concurrentTasks, CancellationToken token)
+        {
+            var extrinsicType = "TransferKeepAlive";
+
+            if (!IsConnected || Account == null)
+            {
+                return null;
+            }
+
+            var multiAddress = new EnumMultiAddress();
+            multiAddress.Create(MultiAddress.Id, to);
+
+            var balance = new BaseCom<U128>();
+            balance.Create(amount);
+
+            var extrinsic = BalancesCalls.TransferKeepAlive(multiAddress, balance);
+
+            return await GenericExtrinsicAsync(Account, extrinsicType, extrinsic, concurrentTasks, token);
+        }
+
+        /// <summary>
+        /// Contracts Call extrinsic
+        /// </summary>
+        /// <param name="dest"></param>
+        /// <param name="value"></param>
+        /// <param name="gasLimit"></param>
+        /// <param name="storageDepositLimit"></param>
+        /// <param name="data"></param>
+        /// <param name="concurrentTasks"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public async Task<string?> ContractsCallAsync(AccountId32 dest, BigInteger value, ulong refTime, ulong proofSize, BigInteger? storageDepositLimit, byte[] data, int concurrentTasks, CancellationToken token)
+        {
+            var extrinsicType = "ContractsCallAsync";
+
+            if (!IsConnected || Account == null)
+            {
+                return null;
+            }
+
+            var destParam = new EnumMultiAddress();
+            destParam.Create(MultiAddress.Id, dest);
+
+            var valueParam = new BaseCom<U128>();
+            valueParam.Create(value);
+
+            var refTimeParam = new BaseCom<U64>();
+            refTimeParam.Create(new CompactInteger(refTime));
+            var proofSizeParam = new BaseCom<U64>();
+            proofSizeParam.Create(new CompactInteger(proofSize));
+            var gasLimitParam = new Weight();
+            gasLimitParam.RefTime = refTimeParam;
+            gasLimitParam.ProofSize = proofSizeParam;
+
+            var storageDepositLimitParam = new BaseOpt<BaseCom<U128>>();
+            if (storageDepositLimit != null)
+            {
+                var storageDepositLimitValue = new BaseCom<U128>();
+                storageDepositLimitValue.Create(storageDepositLimit.Value);
+                storageDepositLimitParam.Create(storageDepositLimitValue);
+            }
+
+            var dataParam = new BaseVec<U8>();
+            dataParam.Create(data.ToU8Array());
+
+            var extrinsic = ContractsCalls.Call(destParam, valueParam, gasLimitParam, storageDepositLimitParam, dataParam);
+
+            return await GenericExtrinsicAsync(Account, extrinsicType, extrinsic, concurrentTasks, token);
+        }
+
+        /// <summary>
+        /// Execute a a bath of extrinsics
+        /// </summary>
+        /// <param name="to"></param>
+        /// <param name="amount"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public async Task<string?> BatchAllAsync(List<EnumRuntimeCall> callList, int concurrentTasks, CancellationToken token)
+        {
+            var extrinsicType = "BatchAll";
+
+            if (!IsConnected || Account == null || callList == null || callList.Count == 0)
+            {
+                return null;
+            }
+
+            var calls = new BaseVec<EnumRuntimeCall>();
+            calls.Create(callList.ToArray());
+
+            var extrinsic = UtilityCalls.BatchAll(calls);
+
+            return await GenericExtrinsicAsync(Account, extrinsicType, extrinsic, concurrentTasks, token);
+        }
+
+    }
+}
