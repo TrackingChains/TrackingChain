24e7566f993364986d2f28581221aef80c1204d2
Federico Cicciarella <federico.cicciarella@gmail.com>
2023-07-26 09:13:05 +0200

    refactory

--- README_SUBSTRATE.md
+++ README_SUBSTRATE.md
@@ -1,4 +1,8 @@
-Example fo command to add new template for specific substrate chain
+For more info visit: 
+    https://github.com/SubstrateGaming/Substrate.Chains.NET
+
+
+Example fo command to add new template for specific substrate chain:
 
     dotnet new install Substrate.DotNet.Template
     dotnet new substrate --sdk_version 0.4.4 --rest_service Substrate.Shibuya.NET.RestService --net_api Substrate.Shibuya.NET.NetApiExt --rest_client Substrate.Shibuya.NET.RestClient --metadata_websocket wss://shibuya.blastapi.io/8508719b-69eb-4c01-92e4-eb902da0fb97 --force --allow-scripts yes
---
+++ src/Substrate.Generic.Client/GenericClient.cs
@@ -0,0 +1,64 @@
+﻿using Schnorrkel.Keys;
+using Substrate.Generic.Client.Helper;
+using Substrate.NetApi;
+using Substrate.NetApi.Model.Types;
+using System.Numerics;
+
+namespace Substrate.Generic.Client
+{
+    internal class GenericClient
+    {
+        private static async Task RunClientAsync(string url, CancellationToken token)
+        {
+
+            var miniSecret = new MiniSecret(Utils.HexToByteArray("0xe5be9a5092b81bca64be81d212e7f2f9eba183bb7a90954f7b76361f6edb5c0a"), ExpandMode.Ed25519);
+            var account = Account.Build(KeyType.Sr25519, miniSecret.ExpandToSecret().ToBytes(), miniSecret.GetPair().Public.Key);
+
+            //Log.Information("Your address: {address}", Utils.GetAddressFrom(account.Bytes, 5));
+
+            var client = new GenericNetwork(account, url);
+
+            if (!await client.ConnectAsync(true, true, token))
+            {
+                //Log.Error("Failed to connect to node");
+                return;
+            }
+
+            //Log.Information("Connected to {url}: {flag}", url, client.IsConnected);
+
+            var blockNumber = await client.GetBlocknumberAsync(token);
+            //Log.Information("Current block is {number}", blockNumber != null ? blockNumber.Value : null);
+
+            // do action in here ...
+            Thread.Sleep(3000);
+
+            var smartContracAddress = "aKpb5m5WBvTA164EdZhkYHU1SHBixY4QPnxbekMDUSfUYGd";
+            var dest = Utils.GetPublicKeyFrom(smartContracAddress).ToAccountId32();
+            var value = new BigInteger(0);
+            var refTime = (ulong)3951114240;
+            var proofSize = (ulong)125952;
+            var storageDepositLimit = new BigInteger(54000000000);
+            var data = Utils.HexToByteArray("0x1ba63d86363617270650000000000000000000000000000000000000000000000000000014616161616100");
+            var subscriptionId = await client.ContractsCallAsync(dest, value, refTime, proofSize, storageDepositLimit, data, 1, token);
+            if (subscriptionId != null)
+            {
+                //Log.Information("SubscriptionId: {subscriptionId}", subscriptionId);
+                var queueInfo = client.ExtrinsicManger.Get(subscriptionId);
+                while (queueInfo != null && !queueInfo.IsCompleted)
+                {
+                    //Log.Information("QueueInfo {subscription} [{state}]", subscriptionId, queueInfo != null ? queueInfo.State.ToString() : queueInfo);
+                    Thread.Sleep(1000);
+                    queueInfo = client.ExtrinsicManger.Get(subscriptionId);
+                }
+            }
+            else
+            {
+                //Log.Error("Failed to call contract");
+            }
+
+
+            await client.DisconnectAsync();
+            //Log.Information("Disconnected from {url}", url);
+        }
+    }
+}
