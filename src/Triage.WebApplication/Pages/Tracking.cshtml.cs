52291dfc672933d6d518ba8a59ca41edbd6da8fb
Federico Cicciarella <federico.cicciarella@gmail.com>
2023-08-06 13:10:35 +0200

    fix interface web application

--- src/Triage.WebApplication/Pages/Tracking.cshtml.cs
+++ src/Triage.WebApplication/Pages/Tracking.cshtml.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Threading.Tasks;
+using TrackingChain.TrackingChainCore.EntityFramework.Context;
 using TrackingChain.TransactionTriageCore.UseCases;
 using TrackingChain.TriageWebApplication.ModelBinding;
 
@@ -14,6 +18,7 @@ namespace TrackingChain.TriageWebApplication.Pages
     public class TrackingModel : PageModel
     {
         // Fields.
+        private readonly ApplicationDbContext dbContext;
         private readonly ILogger<TrackingModel> logger;
         private readonly ITrackingEntryUseCase trackingEntryUseCase;
 
@@ -21,40 +26,38 @@ namespace TrackingChain.TriageWebApplication.Pages
         public string? Result { get; set; }
         [BindProperty]
         public int SelectedChain { get; set; }
-        public SelectList ChainOptions { get; set; }
+        public SelectList ChainOptions { get; set; } = default!;
 
         // Constractor.
         public TrackingModel(
+            ApplicationDbContext dbContext,
             ILogger<TrackingModel> logger,
             ITrackingEntryUseCase trackingEntryUseCase)
         {
+            this.dbContext = dbContext;
             this.logger = logger;
             this.trackingEntryUseCase = trackingEntryUseCase;
-            this.ChainOptions = new SelectList(
-                                    new List<SelectListItem>
-                                    {
-                                        new SelectListItem { Selected = false, Text = "Shibuya - (Astar)", Value = "0"},
-                                        new SelectListItem { Selected = false, Text = "Moonbase - (Moonbeam)", Value = "1"},
-                                    }, "Value", "Text", 1); // TODO configurable
         }
 
         // GET
-        public void OnGet()
+        public async Task OnGet()
         {
-            SelectedChain = 0;
+            await PopulateComboBoxAsync();
         }
 
         // POST
         public async Task OnPostSubmit(TrackProductBinding trackProductBinding)
         {
             ArgumentNullException.ThrowIfNull(trackProductBinding);
+
+            await PopulateComboBoxAsync();
             try
             {
                 var result = await trackingEntryUseCase.AddTransactionAsync(
                     "GenericWabAPI",
-                    trackProductBinding.ProductCode, 
+                    trackProductBinding.ProductCode,
                     trackProductBinding.ProductDataJson,
-                    trackProductBinding.SelectedChain == 0 ? "Shibuya" : "Moonbase");
+                    ChainOptions.ElementAt(trackProductBinding.SelectedChain).Text);
                 Result = $"Status: Ok, Tx Hash: {result}";
             }
 #pragma warning disable CA1031 // 
@@ -63,6 +66,27 @@ namespace TrackingChain.TriageWebApplication.Pages
                 Result = $"Tx Error: {ex.Message}";
             }
 #pragma warning restore CA1031 // 
+        }
+
+        // Helpers.
+        private async Task PopulateComboBoxAsync()
+        {
+            var categories = await dbContext.ProfileGroups
+                .Select(pg => pg.Category)
+                .Distinct()
+                .OrderBy(category => category)
+                .ToListAsync();
+            this.ChainOptions = new SelectList(
+                categories
+                    .Select((category, index) => new SelectListItem
+                    {
+                        Selected = false,
+                        Text = category,
+                        Value = index.ToString(CultureInfo.InvariantCulture)
+                    }),
+                "Value",
+                "Text",
+                1);
         }
     }
 }
