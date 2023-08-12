cd7113f9e3ecacac4025d862bc48237938a49b19
Federico Cicciarella <federico.cicciarella@gmail.com>
2023-08-12 15:03:44 +0200

    Migration DB

--- src/TrackingChain.Core/Migrations/20230812124215_Registry_SmartcontractEndpoint.Designer.cs
+++ src/TrackingChain.Core/Migrations/20230812124215_Registry_SmartcontractEndpoint.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TrackingChain.TrackingChainCore.EntityFramework.Context;
 
@@ -11,9 +12,11 @@ using TrackingChain.TrackingChainCore.EntityFramework.Context;
 namespace TrackingChain.Core.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230812124215_Registry_SmartcontractEndpoint")]
+    partial class Registry_SmartcontractEndpoint
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/TrackingChain.Core/Migrations/20230812124215_Registry_SmartcontractEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrackingChain.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class Registry_SmartcontractEndpoint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+#pragma warning disable CA1062 // Validate arguments of public methods
+            migrationBuilder.AddColumn<string>(
+                name: "SmartContractEndpoint",
+                table: "TransactionRegistries",
+                type: "nvarchar(max)",
+                nullable: true);
+#pragma warning restore CA1062 // Validate arguments of public methods
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+#pragma warning disable CA1062 // Validate arguments of public methods
+            migrationBuilder.DropColumn(
+                name: "SmartContractEndpoint",
+                table: "TransactionRegistries");
+#pragma warning restore CA1062 // Validate arguments of public methods
+        }
+    }
+}
