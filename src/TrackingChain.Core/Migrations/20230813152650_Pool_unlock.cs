11679a157d57f1bc6d4f841488f800e3c0da6238
Federico Cicciarella <federico.cicciarella@gmail.com>
2023-08-13 17:31:10 +0200

    migration

---
+++ src/TrackingChain.Core/Migrations/20230813152650_Pool_unlock.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrackingChain.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class Pool_unlock : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+#pragma warning disable CA1062 // Validate arguments of public methods
+            migrationBuilder.AddColumn<DateTime>(
+                name: "GeneratingFrom",
+                table: "TransactionPools",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+#pragma warning restore CA1062 // Validate arguments of public methods
+
+            migrationBuilder.AddColumn<int>(
+                name: "UnlockTimes",
+                table: "TransactionPools",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UnlockTimes",
+                table: "TransactionPendings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+#pragma warning disable CA1062 // Validate arguments of public methods
+            migrationBuilder.DropColumn(
+                name: "GeneratingFrom",
+                table: "TransactionPools");
+#pragma warning restore CA1062 // Validate arguments of public methods
+
+            migrationBuilder.DropColumn(
+                name: "UnlockTimes",
+                table: "TransactionPools");
+
+            migrationBuilder.DropColumn(
+                name: "UnlockTimes",
+                table: "TransactionPendings");
+        }
+    }
+}
