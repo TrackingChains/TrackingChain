db87952374fe9988bfa9218f4b5d9176948d49f0
Federico Cicciarella <federico.cicciarella@gmail.com>
2023-08-18 18:12:00 +0200

    account and some fixs

--- src/TrackingChain.WebApp/Pages/Admin/Accounts/Index.cshtml.cs
+++ src/TrackingChain.WebApp/Pages/Admin/Accounts/Index.cshtml.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System;
 using System.Threading.Tasks;
 using TrackingChain.TrackingChainCore.Domain.Entities;
 using TrackingChain.TrackingChainCore.EntityFramework.Context;
@@ -13,19 +15,50 @@ namespace TrackingChain.TriageWebApplication.Pages.Admin.Accounts
         public IndexModel(ApplicationDbContext context)
         {
             dbContext = context;
+            NameSort = "";
+            CurrentFilter = "";
+            CurrentSort = "";
         }
 
 #pragma warning disable CA2227 // Collection properties should be read only
         public PaginatedList<Account> Accounts { get; set; } = default!;
 #pragma warning restore CA2227 // Collection properties should be read only
+        //pagination
         public int PageSize { get; set; } = 5;
         public int PageIndex { get; set; } = 1;
         public int TotalItems { get; private set; }
+        //sort and filter
+        public string NameSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync(int pageIndex = 1)
+        public async Task OnGetAsync(
+            string sortOrder,
+            string currentFilter,
+            string searchString,
+            int pageIndex = 1)
         {
-            var query = dbContext.Accounts;
+            // Filter.
+            if (searchString != null)
+                pageIndex = 1;
+            else
+                searchString = currentFilter;
+            CurrentFilter = searchString;
 
+            var query = dbContext.Accounts.AsNoTracking();
+            if (!string.IsNullOrEmpty(searchString))
+                query = query.Where(s => s.Name.StartsWith(searchString));
+
+            // Order.
+            NameSort = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            query = sortOrder switch
+            {
+                "name_desc" => query.OrderByDescending(s => s.Name),
+                _ => query.OrderBy(s => s.Name),
+            };
+            CurrentSort = sortOrder;
+
+            // Pagination.
             TotalItems = await query.CountAsync();
 
             PageIndex = pageIndex;
